3f409171083398f868bc80205c921e38ff69cf99
Darkilling <luis.canales21@inacapmail.cl>
2024-06-09 16:22:07 -0400

    u.u.u.

--- scripts/login.js
+++ scripts/login.js
@@ -3,19 +3,25 @@ const $submit = document.getElementById("submit"),
     $username = document.getElementById("username"),
     $visible = document.getElementById("visible");
 
-document.addEventListener("change", (e) => {
-    if(e.target === $visible){
-        if($visible.checked === false) $password.type = "password";
-        else $password.type = "text";
-    }
-});
-
-
-document.addEventListener("click", (e) => {
-    if(e.target === $submit){
-        if($password.value !== "" && $username.value !== ""){
-            e.preventDefault();
-            window.location.href = "index.html";
+    document.addEventListener("change", (e) => {
+        if(e.target === $visible){
+            if($visible.checked === false) $password.type = "password";
+            else $password.type = "text";
         }
-    }
-});
+    });
+    
+    document.addEventListener("click", (e) => {
+        if(e.target === $submit){
+            e.preventDefault();
+            if($password.value === "" && $username.value !== ""){
+                alert("Por favor, complete el campo de la contraseña");
+            } else if($password.value !== "" && $username.value === ""){
+                alert("Por favor, complete el campo del nombre de usuario");
+            } else if($password.value === "" && $username.value === ""){
+                alert("Por favor, complete los campos del nombre de usuario y la contraseña");
+            } else {
+                window.location.href = "index.html";
+                alert("Bienvenido a Ludoteca  " + $username.value);
+            }
+        }
+    });
